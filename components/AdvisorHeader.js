f03803ee482b011290762f714b87a2023fed65c0
Arora-Sanjay <arorasanjay15397@gmail.com>
2025-09-20 00:37:06 +0530

    Update AdvisorHeader.js

--- components/AdvisorHeader.js
+++ components/AdvisorHeader.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+import { View } from 'react-native';
+import { Avatar, Text } from 'react-native-paper';
+import { styles } from '../src/styles';
+
+export const AdvisorHeader = () => (
+  <View>
+    <Avatar.Icon
+      size={64}
+      icon="robot"
+      style={styles.avatar}
+    />
+    <Text variant="headlineLarge" style={styles.title}>
+      AI Product Advisor
+    </Text>
+    <Text variant="titleMedium" style={styles.subtitle}>
+      Describe your needs in plain English:
+    </Text>
+  </View>
+);
